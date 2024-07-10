5bc3b1082ac3c1d05d24caa9c64286ceb99b68e5
Tech-Matt <mattia.rizzo.un@gmail.com>
2024-07-11 01:12:53 +0200

    debugging graphic library

--- README.md
+++ README.md
@@ -3,6 +3,13 @@ Building a small real time OS for TI MSP432.
 
 ## Supported Functionalities
 
+## Dependencies
+You need to have a Library folder somewhere in your pc to import the following:
+- grlib (see grlib.txt to see the path to add to the include files)
+- Display driver (LcdDriver/Crystalfontz128x128_ST7735.h)
+
+Add here instructions to set up driver lib and grlib 
+
 ## Task Management
 
 ## Scheduling
--- src/main.c
+++ src/main.c
@@ -1,13 +1,29 @@
 #include "msp.h"
 #include "task.h"
 #include "semaphore.h"
-#include "grlib.h"
-#include "LcdDriver/Crystalfontz128x128_ST7735.h"
+#include "Crystalfontz128x128_ST7735.h"
+#include "HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.h"
+#include <ti/grlib/grlib.h>
+
 
 #define LED_DELAY 100000
 
 Semaphore sem;
 
+// Declare global graphics context
+Graphics_Context g_sContext;
+
+void logToLCD(const char* message) {
+    static int yPos = 0;
+
+    if (yPos > 120) {
+        yPos = 0;
+        Graphics_clearDisplay(&g_sContext);
+    }
+
+    Graphics_drawString(&g_sContext, (int8_t *)message, AUTO_STRING_LENGTH, 0, yPos, OPAQUE_TEXT);
+    yPos += 10;
+}
 // Example task 1
 void task1(void) {
     while (1) {
@@ -39,7 +55,15 @@ int main(void) {
     P2->DIR |= BIT1;
     P2->OUT &= ~BIT1;
 
-    // Configure Ports
+    // Initialize LCD
+    Crystalfontz128x128_Init();
+    Crystalfontz128x128_SetOrientation(LCD_ORIENTATION_UP);
+
+    // Initialize graphics context
+    Graphics_initContext(&g_sContext, &g_sCrystalfontz128x128, &g_sCrystalfontz128x128_funcs);
+    Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_WHITE);
+    Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_BLACK);
+    Graphics_clearDisplay(&g_sContext);
 
     initTasks();
     semaphoreInit(&sem, 1);
